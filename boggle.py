f097932225561b977faaa56ef244d6edfe25dafe
KristinEliseLee <KristinEliseLee@gmail.com>
2018-06-27 04:03:12 +0000

    Started effort to add english dictionary in to game

--- boggle.py
+++ boggle.py
@@ -1,5 +1,19 @@
 from random import choice
 import time
+def get_dictionary(file):
+    dictionary = {}
+    with open(file) as whole_dictionary:
+        lines = whole_dictionary.readlines()
+        del lines[:lines.index("-START-\n")+1]
+        del lines[lines.index("-END-\n"):]
+        for line in lines:
+            line.rstrip()
+            index = line.index(" ")
+            word = line[0:index]
+            definition = line[index:].strip()
+            dictionary[word] = definition
+    print(dictionary)
+    return dictionary
 
 def make_board(width):
     all_letters = "AAABCDEEEFGHIIIJKLMNNOOOPQRSSTTUUUVWXYZ"
@@ -63,27 +77,36 @@ def find(board, word):
     return False
 
 def play_game_singleplayer():
-    while True:
-        width = input("How big do you want the board to be? please give 1 number")
-        try:
-            width = int(width)
-            break
-        except ValueError:
-            print("Please type a number")
-
+    print("The rules are simple:")
+    print("You have 60 seconds to type as many words as you can find")
+    print("""3 letter words and shorter are worth 1 point,
+        while longer words are worth 1 extra point per letter over 3""")
+    width = 5
     board = make_board(width)
+    # scrabble_dictionary = get_dictionary("scrabble_dict.txt")
     user_word_set = set()
     now = time.time()
     end = now + 30
     score = 0
-    print("You have 60 seconds, time starts now.")
+    print("The game is about to start")
+    time.sleep(2)
+    print("You have 60 seconds, starting...")
+    time.sleep(1)
+    print("NOW!")
+
     while time.time() < end:
-        user_word_set.add(input("> "))
+        user_word_set.add(input("> ").upper())
+    # for word in user_word_set:
+    #     if word not in scrabble_dictionary:
+    #         print(f"{word} not in dictionary")
+    #         user_word_set.remove(word)
 
     for word in user_word_set:
-        word = word.upper()
         if find(board, word):
-            score += 1
+            if len(word) <= 3:
+                score += 1
+            else:
+                score += len(word)-3
         else:
             print(f"{word} is not on the board")
 
