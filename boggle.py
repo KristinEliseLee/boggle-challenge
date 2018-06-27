217d2efe465141cf7bfe15f3a3ca99ed63288877
Kristin Lee <serpent.kris@gmail.com>
2018-06-26 20:08:32 -0700

    Added single player mode to boggle game

--- boggle.py
+++ boggle.py
@@ -1,109 +1,20 @@
-"""Boggle word check.
+from random import choice
+import time
 
-Given a 5x5 boggle board, see if you can find a given word in it.
-
-In Boggle, you can start with any letter, then move in any NEWS direction.
-You can continue to change directions, but you cannot use the exact same
-tile twice.
-
-So, for example::
-
-    N C A N E
-    O U I O P
-    Z Q Z O N
-    F A D P L
-    E D E A Z
- 
-In this grid, you could find `NOON* (start at the `N` in the top
-row, head south, and turn east in the third row). You cannot find
-the word `CANON` --- while you can find `CANO` by starting at the
-top-left `C`, you can 't re-use the exact same `N` tile on the
-front row, and there's no other `N` you can reach.
-
-For example::
-
-    >>> board = make_board('''
-    ... N C A N E
-    ... O U I O P
-    ... Z Q Z O N
-    ... F A D P L
-    ... E D E A Z
-    ... ''')
-
-`NOON` should be found (0, 3) -> (1, 3) -> (2, 3) -> (2, 4)::
- 
-    >>> find(board, "NOON")
-    True
-
-`NOPE` should be found (0, 3) -> (1, 3) -> (1, 4) -> (0, 4)::
-
-    >>> find(board, "NOPE")
-    True
-
-`CANON` can't be found (`CANO` starts at (0, 1) but can't find
-the last `N` and can't re-use the N)::
-
-    >>> find(board, "CANON")
-    False
-
-You cannot travel diagonally in one move, which would be required
-to find `QUINE`::
-
-    >>> find(board, "QUINE")
-    False
-
-We can recover if we start going down a false path (start 3, 0)::
-
-    >>> find(board, "FADED")
-    True
-
-
-An extra tricky case --- it needs to find the `N` toward the top right,
-and then go down, left, up, up, right to find all four `O`s and the `S`::
-
-    >>> board2 = make_board('''
-    ... E D O S Z
-    ... N S O N R
-    ... O U O O P
-    ... Z Q Z O R
-    ... F A D P L
-    ... ''')
-
-    >>> find(board2, "NOOOOS")
-    True
-    
-"""
-
-
-def make_board(board_string):
-    """Make a board from a string.
-
-    For example::
-
-        >>> board = make_board('''
-        ... N C A N E
-        ... O U I O P
-        ... Z Q Z O N
-        ... F A D P L
-        ... E D E A Z
-        ... ''')
-
-        >>> len(board)
-        5
-
-        >>> board[0]
-        ['N', 'C', 'A', 'N', 'E']
-    """
-
-    letters = board_string.split()
-
-    board = [
-        letters[0:5],
-        letters[5:10],
-        letters[10:15],
-        letters[15:20],
-        letters[20:25],
-    ]
+def make_board(width):
+    all_letters = "AAABCDEEEFGHIIIJKLMNNOOOPQRSSTTUUUVWXYZ"
+    board = []
+    letters_needed = width ** 2
+    board_letters = ""
+    start = 0
+    while len(board_letters) < letters_needed:
+        board_letters += choice(all_letters)
+    for i in range(width):
+        board.append([])
+        board[i].extend(board_letters[start: start + width])
+        start += width
+    for i in range(width):
+        print(" ".join(board[i]))
 
     return board
 
@@ -111,7 +22,7 @@ def make_board(board_string):
 def find(board, word):
     """Can word be found in board?"""
     used_set = set()
-    num_list = [0, 1, 2, 3, 4]
+
 
     def check_next(position, word):
         """Checks current position for letter, then recursively checks neighbors.
@@ -119,7 +30,9 @@ def find(board, word):
         """
         x, y = position
 
-        if x not in num_list or y not in num_list:
+        if x < 0 or x > (len(board)-1):
+            return False
+        elif y < 0 or y > (len(board)-1):
             return False
         elif board[x][y] == word[0].upper() and (x, y) not in used_set:
             if len(word) == 1:
@@ -127,7 +40,7 @@ def find(board, word):
             else:
                 used_set.add(position)
                 neighbor_list = [(x+1, y), (x-1, y), (x, y+1), (x, y-1), 
-                                 (x+1, y+1), (x-1, y+1). (x-1, y+1), (x-1, y-1)]
+                                 (x+1, y+1), (x-1, y+1), (x+1, y-1), (x-1, y-1)]
                 for i in range(len(neighbor_list)):
                     if neighbor_list[i] not in used_set:
                         if check_next(neighbor_list[i], word[1:]) is True:
@@ -141,7 +54,7 @@ def find(board, word):
         return False
 
     for horizonal in range(len(board)):
-        for vertical in range(len(board[0])):
+        for vertical in range(len(board)):
             position = (horizonal, vertical)
             answer = check_next(position, word)
             if answer is True:
@@ -149,9 +62,31 @@ def find(board, word):
 
     return False
 
+def play_game_singleplayer():
+    while True:
+        width = input("How big do you want the board to be? please give 1 number")
+        try:
+            width = int(width)
+            break
+        except ValueError:
+            print("Please type a number")
 
+    board = make_board(width)
+    user_word_set = set()
+    now = time.time()
+    end = now + 30
+    score = 0
+    print("You have 60 seconds, time starts now.")
+    while time.time() < end:
+        user_word_set.add(input("> "))
 
-if __name__ == '__main__':
-    import doctest
-    if doctest.testmod().failed == 0:
-        print ("\n*** ALL TESTS PASSED; YOU FOUND SUCCESS! ***\n")
+    for word in user_word_set:
+        word = word.upper()
+        if find(board, word):
+            score += 1
+        else:
+            print(f"{word} is not on the board")
+
+    print(f"your score is:{score}")
+
+play_game_singleplayer()
